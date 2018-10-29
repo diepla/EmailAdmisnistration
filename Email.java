816860061387af680cad59713068fc97b6a4e9e3
diepla <diepla@my.smccd.edu>
2018-10-29 13:33:57 -0700

    upload email files to repository

---
+++ Email.java
@@ -0,0 +1,64 @@
+package emailapp;
+import java.util.Scanner;
+
+public class Email {
+
+    final static int DEFAULT_MAILBOX_CAPACITY = 20;
+    /** Generate variable for email **/
+    private String firstName;
+    private String lastName;
+    private String department;
+    private String password;
+    private String email;
+    private int mailBoxCapacity;
+    private String companySuffix = ".fluxcompany.com";
+
+    /** Constructors **/
+    public Email(String firstName, String lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.department = getDepartment();
+        this.password = generatePassword(8);
+        mailBoxCapacity = DEFAULT_MAILBOX_CAPACITY;
+        this.email = this.firstName.toLowerCase()  + "." + this.lastName.toLowerCase() + "@"
+                + this.department + companySuffix;
+    }
+
+    /** Prompt department **/
+    private String getDepartment(){
+        System.out.println(this.firstName + ", " + this.lastName + "\nEnter the number according to your apartment:" +
+                "\n1. Sales\n2. Development\n3. Accounting\n");
+        Scanner scan = new Scanner(System.in);
+        int choice = scan.nextInt();
+        if(choice == 1){ return "sales";}
+        else if(choice == 2){return "dev";}
+        else if(choice == 3){return "acc";}
+        else{return "";}
+    }
+
+    /** Generate random string password **/
+    private String generatePassword(int length){
+        String passwordSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890#$&*"; //40
+        char[] password = new char[length];
+        for(int i = 0; i < length; i++){
+            int randomNumber = (int) (Math.random() * passwordSet.length());
+            password[i] = passwordSet.charAt(randomNumber);
+        }
+
+        return new String(password);
+    }
+
+    /** Set the mailbox capacity **/
+    public void setMailBoxCapacity(int num){
+        this.mailBoxCapacity = num;
+    }
+
+    /** Display the info of the email **/
+    public void printedInfo(){
+        System.out.println("Name: " + this.firstName + " " + this.lastName);
+        System.out.println("Email: " + this.email);
+        System.out.println("Password: " + this.password);
+        System.out.println("Mailbox Capacity: " + this.mailBoxCapacity);
+    }
+
+}
---
+++ EmailDrivenClass.java
@@ -0,0 +1,20 @@
+package emailapp;
+
+public class EmailDrivenClass {
+
+    public static void main(String[] args){
+        Email e1 = new Email("Diep", "La");
+        System.out.println("Your email is successfully created");
+        e1.printedInfo();
+
+        System.out.println();
+        Email e2 = new Email("Steve", "Horsley");
+        System.out.println("Your email is successfully created");
+        e2.printedInfo();
+
+        e1.setMailBoxCapacity(50);
+        System.out.println("\nYour mailbox capacity has changed!!");
+        e1.printedInfo();
+
+    }
+}
